3bb907738e9e130de42cc6b5d532a41f703bef62
Gabrielly Lopes <lopesgabrielly475@gmail.com>
2026-06-13 01:38:12 -0300

    Exercício20.java
    Exercício20

--- Lista2/Exercício20.java
+++ Lista2/Exercício20.java
@@ -2,11 +2,28 @@ package Lista20;
 
 import java.util.Scanner;
 
-public class Exerc�cio20 {
+public class Exercício20 {
 
 	public static void main(String[] args) {
 
-	
+	Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Digite um número inteiro: ");
+        int numero = scanner.nextInt();
+        
+       
+        if (numero > 0) {
+            System.out.println("Positivo");
+        } else if (numero < 0) {
+            System.out.println("Negativo");
+        } else {
+            System.out.println("Zero");
+        }
+        
+        
+        scanner.close();
+    }
+
 	}
 
 	}
